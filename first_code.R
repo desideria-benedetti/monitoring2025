51e0518ba097aaec72d7631537fb475c5e9cda2b
desideria-benedetti <benedettidesideria@gmail.com>
2025-05-06 12:43:44 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -5,3 +5,12 @@ library(terra)
 setwd("C:/Users/utente/Desktop/")
 mato = rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+
+# Bande:
+# band1 = near infrared
+# band2 = red
+# band3 = green
+
+plotRGB(mato, r=2, g=3, b=1)
+plotRGB(mato, r=2, g=1, b=3)
+plotRGB(mato, r=1, g=2, b=3)
